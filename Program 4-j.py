9093ba74dcb67729ea9f098b30806de89d9389d5
Seetha002 <145976622+Seetha002@users.noreply.github.com>
2023-11-08 19:52:55 +0530

    Update Program 4-j.py

--- Program 4-j.py
+++ Program 4-j.py
@@ -6,6 +6,7 @@ for i in range(0,2):
     key1=int(input("Enter a key of dict1:"))
     value1=input("Enter value of dict1:")
     dict1[key1]=value1
+for i in range(0,2):
     key2=int(input("Enter a key of dict2:"))
     value2=input("Enter value of dict2:")
     dict2[key2]=value2
