8cdfbda588732253622c6be6b9f170bf1c1ca078
Seetha002 <145976622+Seetha002@users.noreply.github.com>
2023-11-25 11:20:02 +0530

    Create Program 5-h.py

---
+++ Program 5-h.py
@@ -0,0 +1,15 @@
+# In[23]:
+
+
+words=[]
+def longest(word):
+    lw=len(word[0])
+    for i in word:
+        current=len(i)
+        if current>lw:
+            lw=current
+    print("the length of longest word is:",lw)
+for i in range(0,4):
+    n=input("Enter a word:")
+    words.append(n)
+longest(words)
