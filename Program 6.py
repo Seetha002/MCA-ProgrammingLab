1c0b42a734246fb039c3eb23d53f8cec2061dffa
Seetha002 <145976622+Seetha002@users.noreply.github.com>
2023-11-25 11:04:23 +0530

    Update Program 6.py

--- Program 6.py
+++ Program 6.py
@@ -20,30 +20,6 @@ result=factorial(num)
 print("The factorial of given number:",result)
 
 
-# In[28]:
-
-
-def fibonacci(n):
-    a=0
-    b=1
-    fib=0
-    if n==0:
-        return a
-    elif n==1:
-        return b
-    else:
-        print(a)
-        print(b)
-        for i in range(2,n):
-            fib=a+b
-            a=b
-            b=fib
-            print(fib)
-num=int(input("Enter a number"))
-print("The fibnoacci series for given number",num,"is:")
-fibonacci(num)
-
-
 # In[34]:
 
 
